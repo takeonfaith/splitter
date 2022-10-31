e819b84cd83246e0433e388565ddad95d0385b83
Konstantin Doloz <takeonfaith6@gmail.com>
2022-10-31 14:45:03 +0300

    eslint

--- src/pages/split-bill/index.tsx
+++ src/pages/split-bill/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from "react";
 import { Input } from "../../common/input";
 import { User } from "../../components/user";
