8fe7e1b9b2715cd5aefc57afabd649f7d8984b15
Konstantin Doloz <takeonfaith6@gmail.com>
2022-11-03 19:39:31 +0300

    divider tiny fix

--- src/common/divider/divider.ts
+++ src/common/divider/divider.ts
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const DividerStyled = styled.div<{ margin?: string }>`
   width: 100%;
-  height: 1px;
+  height: 0.5px;
   margin: ${({ margin }) => margin ?? "8px 0"};
   background: var(--tg-theme-hint-color);
-  opacity: 0.8;
+  opacity: 0.6;
 `;
 
 export default DividerStyled;
